b43a5acf6a79e076b7423c08e406b5e5b29aec4a
Ubloddy <suryaprakash.sp987@gmail.com>
2020-09-10 22:35:12 +0530

    module4Assignment

---
+++ Module4-Assignment/js/script.js
@@ -0,0 +1,18 @@
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry","Paula", "Laura", "Jim"];
+for(i=0; i<10; i++){
+	if(names[i] == "John"){
+		console.log("Goodbye " + names[i])
+	}
+	else if(names[i] == "Jen"){
+		console.log("Goodbye " + names[i])
+	}
+	else if(names[i] == "Jason"){
+		console.log("Goodbye " + names[i])
+	}
+	else if(names[i] == "Jim"){
+		console.log("Goodbye " + names[i])
+	}
+	else{
+		console.log("Hello" + " " + names[i])
+	}
+}
